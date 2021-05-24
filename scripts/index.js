e41ddf5791bb44404250c424b76420520ca48615
Sofia Alekseeva <sofia1alekseeva@yandex.ru>
2021-05-24 17:27:50 +0300

    ready for review

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,37 @@
-const editProfile = document.querySelector('.profile__info_edit-button');
+const popupOpen = document.querySelector('.profile__info_edit-button');
 const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close')
+const popupClose = popup.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('input.popup__input');
+const jobInput = document.querySelector('input.popup__input:nth-child(2)');
+const profileName = document.querySelector('.profile__info_title');
+const jobName = document.querySelector('.profile__info_subtitle');
 
-editProfile.addEventListener('click', function () {
-  console.log('Мы кликнули по элементу');
+
+function similarValue() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = jobName.textContent;
+}
+
+popupOpen.addEventListener("click", toggleClass, false);
+popupOpen.addEventListener("click", similarValue, false);
+popupClose.addEventListener("click", toggleClass, false);
+
+function toggleClass() {
   popup.classList.toggle('popup__opened');
+}
+
+popup.addEventListener('click', function (event) {
+  if (event.target === event.currentTarget) {
+    toggleClass();
+  }
 });
 
-popupClose.addEventListener('click', function () {
-  popup.classList.toggle('popup__opened')
-});
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  jobName.textContent = jobInput.value;
+
+  toggleClass();
+}
+formElement.addEventListener('submit', formSubmitHandler);
