217516570f3ff16aca1af6ca920f243b7c6a4515
Sofia Alekseeva <sofia1alekseeva@yandex.ru>
2021-05-24 17:59:23 +0300

    ready for review

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-const popupOpen = document.querySelector('.profile__info_edit-button');
+const popupOpen = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const popupClose = popup.querySelector('.popup__close');
 const formElement = document.querySelector('.popup__container');
 const nameInput = document.querySelector('input.popup__input');
 const jobInput = document.querySelector('input.popup__input:nth-child(2)');
-const profileName = document.querySelector('.profile__info_title');
-const jobName = document.querySelector('.profile__info_subtitle');
+const profileName = document.querySelector('.profile__title');
+const jobName = document.querySelector('.profile__subtitle');
 
 
 function similarValue() {
